381ab7b38be3a92078b09edc3ae3525fa800ff47
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2025-04-19 12:35:47 +0530

    init frontend and tailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='text-3xl text-red-500'>
+      aagbvb
+    </div>
+    Hii there
+    </>
+  )
+}
+
+export default App
